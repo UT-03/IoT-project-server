fc624e403170255771d3aae17fe6f7aeadf2573b
UT-03 <f20200458@pilani.bits-pilani.ac.in>
2022-12-13 09:43:34 +0530

    feat: add view engine

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const router = require("./routes");
 
 const PORT = 8080;
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}...`);
